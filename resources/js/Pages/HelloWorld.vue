4b84c4c3fe58389f84992f7509dc44151651a44e
tal <talevi100@gmail.com>
2023-08-31 16:03:53 +0200

    first commit

---
+++ resources/js/Pages/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Hello World!</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HelloWorld",
+    props: {
+        message: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/js/app.js',
+                // Add your stylesheet here
+            ],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@components': '/resources/js/Components',
+            '@layouts': '/resources/js/Layouts',
+            '@pages': '/resources/js/Pages'
+        },
+    },
+});
